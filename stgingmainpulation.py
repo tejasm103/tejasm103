76399d0b9bad508f84f2fce4664b14df4de43b27
TEJAS  M <tejasmteju310@gmail.com>
2026-09-10 23:19:54 +0530

    Add files via upload

---
+++ stgingmainpulation.py
@@ -0,0 +1,23 @@
+#string add
+'''firstname=input("enter the name:\n ")
+lastname=input("enter the name:\n ")
+full_name=(f"{firstname}  {lastname} ")
+print(full_name)'''
+#repetition
+message=" This Is The Warning \n"
+print(message*2)
+#string method
+#1method
+print(message.upper())
+#lower case
+print(message.lower())
+#strip
+print(message.strip()*2)
+#replace
+print(message.replace("warning","error"))
+#length
+print(len(message))
+#accessing string characters
+print(message[7])
+print(message[3:14])
+print(message[0:19:3])
