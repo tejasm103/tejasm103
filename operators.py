305b9356a0a5baa7e2eaa901ba8e4ed7604785d3
TEJAS  M <tejasmteju310@gmail.com>
2026-09-16 21:24:01 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,40 @@
+#assigment operators
+a=2
+b=3
+a+=10#short form of a=a+10
+print(a)
+b+=10
+print(b)
+a-=10
+print(a)
+b-=10
+print(b)
+a*=10
+print(a)
+b*=10
+print(b)
+a/=10
+print(a)
+b/=10
+print(b)
+#compariosn
+print("-------------------")
+print(a==b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+print(a!=b)
+#logical operators
+print("-------------------")
+print(1>2 and 2<3)
+print(1>2 or 2<3)
+print(not 2>3)
+#membership operators
+print("-------------------")
+s="tejas"
+s2="tejasm"
+print(("t"in s) and ("a"in s2 ))
+print(("t"in s) or ("h"in s2 ))
+print(not("t"in s))
+#bitwise operators
